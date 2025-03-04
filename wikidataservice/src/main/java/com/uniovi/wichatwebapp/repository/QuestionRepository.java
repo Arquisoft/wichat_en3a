9e0d0190f7dcfba7f9e91144596adeb4b7b92589
lauralaca <uo277510@uniovi.es>
2025-03-04 17:19:03 +0100

    Created initial package hierarchy

---
+++ wikidataservice/src/main/java/com/uniovi/wichatwebapp/repository/QuestionRepository.java
@@ -0,0 +1,4 @@
+package main.java.com.uniovi.wichatwebapp.repository;
+
+public class QuestionRepository {
+}
---
+++ wikidataservice/src/main/java/com/uniovi/wichatwebapp/service/QuestionService.java
@@ -0,0 +1,4 @@
+package main.java.com.uniovi.wichatwebapp.service;
+
+public class QuestionService {
+}
