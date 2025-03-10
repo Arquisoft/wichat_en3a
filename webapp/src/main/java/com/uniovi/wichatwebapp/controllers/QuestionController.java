dd9e434e0ec3f742575d75519f277ff92010ce88
lauralaca <uo277510@uniovi.es>
2025-03-10 20:40:38 +0100

    Working all button clicks, not endpoints

--- webapp/src/main/java/com/uniovi/wichatwebapp/controllers/QuestionController.java
+++ webapp/src/main/java/com/uniovi/wichatwebapp/controllers/QuestionController.java
@@ -2,6 +2,7 @@ package com.uniovi.wichatwebapp.controllers;
 
 import com.uniovi.wichatwebapp.entities.Question;
 import com.uniovi.wichatwebapp.services.QuestionService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +21,11 @@ public class QuestionController {
                 value = {"/game/question"},
             method = {RequestMethod.GET}
     )
-    public String getQuestion(Model model) {
+    public String getQuestion(Model model, HttpSession session) {
+
         Question question = questionService.getRandomQuestion();
         model.addAttribute("question", question);
+        session.setAttribute("question", question);
         return "question/question";
     }
 
@@ -30,8 +33,8 @@ public class QuestionController {
             value = {"/game/chooseAnswer/{id}"},
             method = {RequestMethod.GET}
     )
-    public String chooseAnswer(@PathVariable String id, Model model) {
-        Question question = (Question)model.getAttribute("question");
+    public String chooseAnswer(@PathVariable String id, HttpSession session) {
+        Question question = (Question)session.getAttribute("question");
         if(questionService.checkAnswer(id, question)){
             questionService.removeQuestion(question);
             return "redirect:/game/question/";
