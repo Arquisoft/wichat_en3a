09d2218e7dc6d33f12ba46491d1a3c0bdc733340
UO294067 <uo294067@uniovi.es>
2025-04-01 02:45:48 +0200

    HintController test for webapp

---
+++ webapp/src/test/java/com/uniovi/wichatwebapp/controllers/HintControllerTests.java
@@ -0,0 +1,43 @@
+package com.uniovi.wichatwebapp.controllers;
+
+import com.uniovi.wichatwebapp.entities.Answer;
+import com.uniovi.wichatwebapp.entities.Question;
+import com.uniovi.wichatwebapp.services.GameService;
+import com.uniovi.wichatwebapp.services.HintService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class HintControllerTests {
+    @InjectMocks
+    private HintController hintController;
+
+    @Mock
+    private HintService hintService;
+
+    @Mock
+    private GameService gameService;
+
+    @Test
+    void getHintTest() {
+        //Mock a question of the game service
+        String questionFromUser="In which continent it is?";
+        String correctAnswer="Madrid";
+        Question question = new Question(new Answer(correctAnswer,"en"), "Which is the capital of Spain?", "no-image");
+        question.setId("1234");
+
+        when(gameService.getCurrentQuestion()).thenReturn(question);
+
+        //Ask controller
+        String hint = hintController.getHint(questionFromUser);
+
+        Assertions.assertFalse(hint.contains(correctAnswer), "The response should not contain the answer to the question");
+        Assertions.assertTrue(hint.length()<500, "The response should be as short as possible");
+    }
+}
